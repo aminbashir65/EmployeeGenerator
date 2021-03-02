2b4c17f6b06ad9fc917f26f1defb89a5a6220bf2
amin bashir <aminbashir65@gmail.com>
2021-03-02 13:40:17 -0600

    added changes to the app js

--- app.js
+++ app.js
@@ -76,4 +76,59 @@ async function question(){
                 message: 'What role does this employee have? : ',
                 choices: ['Engineer', 'Intern', 'Manager']
             }
-        ])
+        ]);
+    let response2= '';
+        if (response.role === 'Engineer'){
+            response2= await inquirer.prompt([{
+                type: 'input',
+                name:'github',
+                message: 'What is the github username for employee?: ',
+            }]);
+            const engineer= new Engineer(response.name, response.id, response.email, response2.github);
+            teamArray.push(engineer);
+        }
+        else if (response.role === 'Intern'){
+            response2= await inquirer.prompt([{
+                type: 'input',
+                name: 'school',
+                message: 'What school is employee currently attending?: '    
+            }]);
+            const intern= new Intern(response.name, response.id, response.email, response2.school);
+            teamArray.push(intern);
+        }
+        else if (response.role === 'Manager'){
+            response2 = await inquirer.prompt([{
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the office number for the employee?: '
+            }]);
+            const manager= new Manager(response.name, response.id, response.email, response2.officeNumber);
+            teamArray.push(manager);
+        }
+
+    }
+    catch(err){
+        return console.log(err);
+    }
+     console.log(teamArray)
+    formComplete= await inquirer.prompt([{
+        type:'list',
+        name: 'finish',
+        message: 'Would you like to add another employee?: ',
+        choices:[
+            'Yes',
+            'No'
+        ]
+    }]);
+    } 
+    while (formComplete.finish === "Yes"){
+        fs.writeFile(outputPath, render(teamArray), function(err){
+            if (err){
+                console.log(err);
+            }
+            else{
+            console.log('Successfully created team!')
+            }
+        })
+    }
+};
